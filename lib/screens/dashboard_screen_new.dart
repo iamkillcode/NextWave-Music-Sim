5f8f8f91d1e91413099e7f9cd3a2e64b649b9e53
Emmanuel Oppong <juniorpara1414@gmail.com>
2025-10-19 23:04:28 +0000

    Update fanbase formatting to 1 decimal place (1.2K, 20.0K, 1.2M)

--- lib/screens/dashboard_screen_new.dart
+++ lib/screens/dashboard_screen_new.dart
@@ -1687,9 +1687,9 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   String _formatNumber(int number) {
     if (number >= 1000000) {
-      return '${(number / 1000000).toStringAsFixed(2)}M';
+      return '${(number / 1000000).toStringAsFixed(1)}M';
     } else if (number >= 1000) {
-      return '${(number / 1000).toStringAsFixed(2)}K';
+      return '${(number / 1000).toStringAsFixed(1)}K';
     } else {
       return '$number';
     }
